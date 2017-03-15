a1c0dcab37ae4d4cf17d75f16adb129d1431a3eb
mybookszzz <136358276@qq.com>
2017-03-15 14:56:57 +0800

    java install for linux

---
+++ java_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#author:zhang
+#date:2017-03-15
+
+Java_src_dir=/opt/src/java$RANDOM
+
+which java || { echo "java is not install"; }
+
+mkdir $Java_src_dir
+cd $Java_src_dir
+echo "please upload jdk.tar.gz..."
+rz -bey
+tar -zxvf jdk*.tar.gz
+Java_Name=`ls -l | grep ^d | awk '{print $9}'`
+cp -rf $Java_Name /usr/local/jdk
+
+cat >>/etc/profile<<"EOF"
+
+
+#Java set
+JAVA_HOME=/usr/local/jdk
+CLASSPATH=$JAVA_HOME/lib/
+PATH=$PATH:$JAVA_HOME/bin
+export PATH JAVA_HOME CLASSPATH
+EOF
